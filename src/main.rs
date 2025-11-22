9979c9e95df19cc307e801fc9943411d17965c8c
goll <joaog.bastos@protonmail.ch>
2025-11-22 14:17:24 -0300

    Initial bevy project setup

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,28 @@
-use std::fs::File;
-use std::io::Write;
-use std::time::SystemTime;
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
-use nalgebra::Vector2;
+use bevy::window::PresentMode;
 
 mod brachistochrone;
-
 use brachistochrone::Brachistochrone;
 
 fn main() {
-    let ks = [
-        (10, 3.),
-        (20, 5.),
-        (50, 7.),
-        (100, 15.),
-        (150, 30.),
-        (200, 40.),
-        (500, 50.),
-    ];
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Brachistochrone Demo".into(),
+                name: Some("brachistochrone-dp-bevy".into()),
+                present_mode: PresentMode::AutoVsync,
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, setup_graphics)
+        .run();
+}
 
-    for (k, inc) in ks {
-        let mut start = Vector2::<f32>::new(0., k as f32);
-        let end = Vector2::<f32>::new(k as f32, 0.);
-
-        let mut b = Brachistochrone::new(k, 10. / (k as f32), start, end);
-
-        println!("Solving Brachistochrone for n={k}");
-
-        let before = SystemTime::now();
-        b.solve();
-
-        println!(" >> Took {}s", before.elapsed().unwrap().as_secs_f64());
-
-        while start.x < k as f32 / 2. {
-            let path = format!("brac-{}-x{}.csv", k, start.x);
-            println!(
-                " :: Iterating path starting from [{:2.2}, {:2.2}] -> {path}",
-                start.x, start.y
-            );
-
-            let mut f = File::options().write(true).create(true).open(path).unwrap();
-
-            for (t, r) in b.path_iter(start) {
-                write!(f, "{},{},{}\n", r.x, r.y, t).unwrap();
-            }
-
-            write!(f, "{},{},{}\n", end.x, end.y, 0).unwrap();
-
-            start.x += inc;
-        }
-    }
+fn setup_graphics(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
 }
